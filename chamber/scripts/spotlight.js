b17fb2851d63047baf45c7d63ffc2f819b71759b
Carin Y Gardner <cariny.gardner@gmail.com>
2025-05-29 20:25:19 -0700

    spotlight functions, css on larger pages

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -4,10 +4,10 @@ const cards = document.querySelector("#spotlight-cards");
 async function getSpotlightData() {
     const response = await fetch(file);
     const data = await response.json();
-    console.table(data.members);
-    const selectedMembers = selectRandomMembers(data);
-    console.log(selectedMembers);
-    
+    // console.table(data.members);
+    const selectedMembers = selectRandomMembers(data.members);
+    // console.log(selectedMembers);
+    displaySelectedMembers(selectedMembers);
 }
 
 function selectRandomMembers(memberList) {
@@ -30,7 +30,37 @@ function selectRandomMembers(memberList) {
     return [...selectedGold, selectedSilver];
 }
 
+const displaySelectedMembers = (members) => {
+    members.forEach(member => {
+        let card = document.createElement('section');
+        let businessName = document.createElement('h2');
+        let address = document.createElement('p');
+        let spanReed = document.createElement('p');
+        let proprietor = document.createElement('p');
+        let icon = document.createElement('img');
+        let website = document.createElement('a');
 
+        businessName.textContent = member.name;
+        icon.setAttribute('src', member.image);
+        icon.setAttribute('alt', `Icon for ${member.name} provided by Icons8`);
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '64');
+        icon.setAttribute('height', '64');
+        address.innerHTML = `<strong>Found in:</strong> ${member.address}`;
+        spanReed.innerHTML = `<strong>Span Reed ID#:</strong> ${member.phone}`;
+        proprietor.innerHTML = `<strong>Contact:</strong> ${member.proprietor} from ${member.country}`;
+        website.setAttribute('href', '#');
+        website.textContent = member.URL;
 
+        card.appendChild(businessName);
+        card.appendChild(icon);
+        card.appendChild(address);
+        card.appendChild(spanReed);
+        card.appendChild(proprietor);
+        card.appendChild(website);
+        cards.appendChild(card);
+    });
+
+}
 
 getSpotlightData();
