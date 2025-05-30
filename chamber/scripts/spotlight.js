131c85858696165b1d3171f2cc074bba3c78a353
Carin Y Gardner <cariny.gardner@gmail.com>
2025-05-29 17:00:18 -0700

    working on random selection of members

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -5,16 +5,32 @@ async function getSpotlightData() {
     const response = await fetch(file);
     const data = await response.json();
     console.table(data.members);
-    // displayRandomGold(data.members);
-    // displayRandomSilver(data.members);
-}
-
-getSpotlightData();
-
-function displayRandomGold() {
+    const selectedMembers = selectRandomMembers(data);
+    console.log(selectedMembers);
     
 }
 
-function displayRandomSilver() {
+function selectRandomMembers(memberList) {
+    const goldMembers = memberList.filter(member => member.level === "gold");
+    const silverMembers = memberList.filter(member => member.level === "silver");
 
+    const selectedGold = [];
+    const goldCopy = [...goldMembers];
+
+    const randomGold1 = Math.floor(Math.random() * goldCopy.length);
+    selectedGold.push(goldCopy[randomGold1]);
+    goldCopy.splice(randomGold1, 1);
+
+    const randomGold2 = Math.floor(Math.random() * goldCopy.length);
+    selectedGold.push(goldCopy[randomGold2]);
+
+    const randomSilver = Math.floor(Math.random() * silverMembers.length);
+    const selectedSilver = silverMembers[randomSilver];
+
+    return [...selectedGold, selectedSilver];
 }
+
+
+
+
+getSpotlightData();
