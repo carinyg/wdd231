8bb5e57e253acfa6667a9dec30d712f7d2778c06
Carin Y Gardner <cariny.gardner@gmail.com>
2025-06-12 15:56:11 -0700

    footer working

--- project/scripts/ck-sourdough.mjs
+++ project/scripts/ck-sourdough.mjs
@@ -1,4 +1,8 @@
 
 import { toggleMenu } from "./toggle-menu.mjs";
+import { updateFooter } from "./update-footer.mjs";
 
 document.querySelector('#menu').addEventListener('click', toggleMenu);
+document.addEventListener('DOMContentLoaded', () => {
+    updateFooter();
+});
