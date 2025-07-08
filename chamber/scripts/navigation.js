4d0bd870a7b0a8bbf0145fdd8afa69da10f6f5b9
Carin Y Gardner <cariny.gardner@gmail.com>
2025-07-08 13:02:31 -0700

    moved directory files from wdd231-previous attempt repository

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,7 @@
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.navigation');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
