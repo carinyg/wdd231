733c7759987d5c1411893da1586c0aaed88a42b2
Carin Y Gardner <cariny.gardner@gmail.com>
2025-07-16 09:56:55 -0700

    commented out console.log

--- week03/scripts/weather-map.js
+++ week03/scripts/weather-map.js
@@ -14,7 +14,7 @@ async function apiFetch() {
         const response = await fetch(myURL);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            // console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
