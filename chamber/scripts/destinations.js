3e6edea2bb823b2023bf897da9aafb8cf16340a6
Carin Y Gardner <cariny.gardner@gmail.com>
2025-08-14 13:23:19 -0700

    commented out a console.log

--- chamber/scripts/destinations.js
+++ chamber/scripts/destinations.js
@@ -4,7 +4,7 @@ const cards = document.querySelector('#destination-cards');
 async function getDestinationData() {
     const response = await fetch(file);
     const data = await response.json();
-    console.table(data.destinations);
+    // console.table(data.destinations);
     displayDestinations(data.destinations);
 }
 
